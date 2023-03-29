419f8753a674ad625b9748455af252590fde9fe4
Matheus Oliveira <matheusfilho98.ms@gmail.com>
2023-03-28 21:17:09 -0300

    chore: configure project using localstack, jest...

---
+++ demo-04-tooling-jest-localstack-plus/tooling-jest-docker/__tests__/integration/s3bucket.test.js
@@ -0,0 +1,37 @@
+const {
+  describe,
+  test,
+  expect,
+  beforeAll,
+  afterAll,
+} = require("@jest/globals");
+const { main } = require("../../src");
+const { S3 } = require("../../src/factory");
+
+describe("Testing AWS Servicess Offline with LocalStack", () => {
+  const bucketConfig = {
+    Bucket: "test",
+  };
+
+  beforeAll(async () => {
+    await S3.createBucket(bucketConfig).promise();
+  });
+
+  afterAll(async () => {
+    await S3.deleteBucket(bucketConfig).promise();
+  });
+
+  test("it should return an array with S3 Bucket", async () => {
+    const expected = bucketConfig.Bucket;
+
+    const response = await main();
+
+    const {
+      allBuckets: { Buckets },
+    } = JSON.parse(response.body);
+
+    const { Name } = Buckets.find(({ Name }) => Name === expected);
+    expect(Name).toStrictEqual(expected);
+    expect(response.statusCode).toStrictEqual(200);
+  });
+});
